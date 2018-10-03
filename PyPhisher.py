217b58bdb306a92d8b03c0f44cef624d270133d3
Daniel Bloom <daniel@bcdefense.com>
2018-10-02 20:19:03 -0500

    testing

--- PyPhisher.py
+++ PyPhisher.py
@@ -244,6 +244,9 @@ class Sender():
 
                 if log:
                     Writer.Log("Sending email to: " + msg['To'] + " from: " + msg['From'])
+                print msg['To']
+                print msg['From']
+                
                 smtp.sendmail(msg['To'], msg['From'], msg.as_string())
 
                 if output:
